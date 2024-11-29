1022cb8d7eaa2c380a3064501ed2e63e61a03b2e
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-29 22:02:20 +0200

    add start

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const SideBar = () => {
+  const linkStyle =
+    "bg-slate-200 p-2 rounded-md border border-transparent hover:bg-transparent hover:border hover:border-slate-200";
   return (
-    <div className="flex flex-col gap-5 p-5 bg-slate-100 w-[20%]">
-      <Link to={"/"}>home</Link>
-      <Link to={"/cars"}>cars</Link>
-      <Link to={"/motorcycle"}>motorcycle</Link>
-      <Link to={"/customers"}>customers</Link>
-      <Link to={"/"}>home</Link>
+    <div className="flex flex-col gap-5 p-5 bg-slate-100 w-[20%] h-[100vh]">
+      <Link to={"/"} className={linkStyle}>
+        home
+      </Link>
+      <Link to={"/cars"} className={linkStyle}>
+        cars
+      </Link>
+      <Link to={"/motorcycle"} className={linkStyle}>
+        motorcycle
+      </Link>
+      <Link to={"/customers"} className={linkStyle}>
+        customers
+      </Link>
+      <Link to={"/"} className={linkStyle}>
+        home
+      </Link>
     </div>
   );
 };
--- src/views/Cars.jsx
+++ src/views/Cars.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Cars = () => {
-  return <div className="w-[50%]"> car</div>;
+  return (
+    <div className="flex-1 md:p-5">
+      <h2>cars</h2>
+      <table className="table-auto border w-full border-collapse">
+        <tr className="border">
+          <th className="border">owner</th>
+          <th className="border">type</th>
+          <th className="border">model</th>
+          <th className="border">plate number</th>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default Cars;
