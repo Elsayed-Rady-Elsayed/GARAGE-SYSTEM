09168f11aee0474bf5ba20901ca49dc4ea3821c0
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-30 22:43:09 +0200

    add start

--- src/views/customers/AddCustomer.jsx
+++ src/views/customers/AddCustomer.jsx
@@ -1,7 +1,83 @@
 import React from "react";
 
 const AddCustomer = () => {
-  return <div>AddCustomer</div>;
+  return (
+    <div className="p-5 w-full">
+      <h2 className="text-3xl font-bold">add new motor</h2>
+      <form action="" className="grid grid-cols-1 md:grid-cols-2 gap-3 mt-7">
+        <div className="flex flex-col gap-3">
+          <label htmlFor="name" className="capitalize font-semibold">
+            owner name
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder="owner"
+            className="border p-1 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="type" className="capitalize font-semibold">
+            motor type
+          </label>{" "}
+          <input
+            id="type"
+            type="text"
+            placeholder="motor type"
+            className="border p-1 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="model" className="capitalize font-semibold">
+            motor model
+          </label>{" "}
+          <input
+            id="model"
+            type="text"
+            placeholder="motor model"
+            className="border p-1 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="plateNumber" className="capitalize font-semibold">
+            plate number
+          </label>{" "}
+          <input
+            id="plateNumber"
+            type="text"
+            placeholder="motor plate"
+            className="border p-1 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="plateCode" className="capitalize font-semibold">
+            plate code
+          </label>{" "}
+          <input
+            id="plateCode"
+            type="text"
+            placeholder="plate code"
+            className="border p-1 rounded-md"
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label htmlFor="subscripation" className="capitalize font-semibold">
+            monthly subscription
+          </label>{" "}
+          <input
+            id="subscripation"
+            type="text"
+            placeholder="monthly subscription"
+            className="border p-1 rounded-md"
+          />
+        </div>
+        <input
+          type="submit"
+          className="bg-slate-500 p-2 rounded-md text-white cursor-pointer w-1/2"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default AddCustomer;
--- src/views/customers/Customers.jsx
+++ src/views/customers/Customers.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Customers = () => {
-  return <div></div>;
+  return (
+    <div className="flex-1 md:p-5">
+      <div className="flex justify-between mb-5">
+        <h2 className="text-3xl font-bold">customers</h2>
+        <Link
+          to={"/addCustomer"}
+          className="bg-slate-500 text-white p-2 rounded-md"
+        >
+          add new customer
+        </Link>
+      </div>
+      <table className="table-auto border w-full border-collapse">
+        <tr className="border">
+          <th className="border">name</th>
+          <th className="border">id</th>
+          <th className="border">gender</th>
+          <th className="border">vehicle type</th>
+          <th className="border">vehicle brand</th>
+          <th className="border">monthly subscription</th>
+          <th className="border">subscription date</th>
+        </tr>
+        <tr key="" className="border">
+          <td className="border text-center">elsayed</td>
+          <td className="border text-center">3232332</td>
+          <td className="border text-center">male</td>
+          <td className="border text-center">car</td>
+          <td className="border text-center">hunda</td>
+          <td className="border text-center">622</td>
+          <td className="border text-center">25/12</td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default Customers;
