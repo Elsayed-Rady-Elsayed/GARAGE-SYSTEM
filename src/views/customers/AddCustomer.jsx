87058a117ffb9a6d4f7d45f94f9714bdeef9a0fd
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-12-11 20:23:30 +0200

    angular day2

--- src/views/customers/AddCustomer.jsx
+++ src/views/customers/AddCustomer.jsx
@@ -7,56 +7,59 @@ const AddCustomer = () => {
       <form action="" className="grid grid-cols-1 md:grid-cols-2 gap-3 mt-7">
         <div className="flex flex-col gap-3">
           <label htmlFor="name" className="capitalize font-semibold">
-            owner name
+            name
           </label>
           <input
             id="name"
             type="text"
-            placeholder="owner"
+            placeholder="name"
             className="border p-1 rounded-md"
           />
         </div>
         <div className="flex flex-col gap-3">
-          <label htmlFor="type" className="capitalize font-semibold">
-            motor type
+          <label htmlFor="id" className="capitalize font-semibold">
+            id
           </label>{" "}
           <input
-            id="type"
+            id="id"
             type="text"
-            placeholder="motor type"
+            placeholder="id"
             className="border p-1 rounded-md"
           />
         </div>
         <div className="flex flex-col gap-3">
-          <label htmlFor="model" className="capitalize font-semibold">
-            motor model
-          </label>{" "}
-          <input
-            id="model"
-            type="text"
+          <label htmlFor="gender" className="capitalize font-semibold">
+            gender
+          </label>
+          <select
+            name="gender"
+            id="gender"
             placeholder="motor model"
             className="border p-1 rounded-md"
-          />
+          >
+            <option value="male">male</option>
+            <option value="female">female</option>{" "}
+          </select>
         </div>
         <div className="flex flex-col gap-3">
-          <label htmlFor="plateNumber" className="capitalize font-semibold">
-            plate number
+          <label htmlFor="vehicleType" className="capitalize font-semibold">
+            vehicle type
           </label>{" "}
           <input
-            id="plateNumber"
+            id="vehicleType"
             type="text"
-            placeholder="motor plate"
+            placeholder="vehicle type"
             className="border p-1 rounded-md"
           />
         </div>
         <div className="flex flex-col gap-3">
-          <label htmlFor="plateCode" className="capitalize font-semibold">
-            plate code
+          <label htmlFor="vehiclebrand" className="capitalize font-semibold">
+            vehicle brand
           </label>{" "}
           <input
-            id="plateCode"
+            id="vehiclebrand"
             type="text"
-            placeholder="plate code"
+            placeholder="vehicle brand"
             className="border p-1 rounded-md"
           />
         </div>
@@ -71,6 +74,21 @@ const AddCustomer = () => {
             className="border p-1 rounded-md"
           />
         </div>
+        <div className="flex flex-col gap-3">
+          <label
+            htmlFor="subscriptiondate"
+            className="capitalize font-semibold"
+          >
+            subscription date
+          </label>{" "}
+          <input
+            id="subscriptiondate"
+            type="text"
+            placeholder="subscription date"
+            className="border p-1 rounded-md"
+          />
+        </div>
+        <div></div>
         <input
           type="submit"
           className="bg-slate-500 p-2 rounded-md text-white cursor-pointer w-1/2"
